e3b0119401507eac1c8c01057951e6b56f2b866f
Nathan-Anhaia <55770434+Nathan-Anhaia@users.noreply.github.com>
2019-11-05 13:38:13 -0300

    Update cliente.h

--- Banco/Libs/cliente.h
+++ Banco/Libs/cliente.h
@@ -1,14 +1,27 @@
-#ifndef CLIENTE_H
-#define CLIENTE_H
-typedef struct Cliente
+#ifndef CLIENTE_H_INCLUDED
+#define CLIENTE_H_INCLUDED
 
+typedef struct Cliente
 {
 
-char nome[10];
-char cpf[11];
-char telefone[10];
+    char nome[10];
+    char cpf[11];
+    char telefone[10];
 
 }TCliente;
 
-void imprimirCliente(TCliente cliente);
-#endif
+void imprimirCliente(TCliente cliente){
+
+    for (int i =0; i<10; i++){
+
+        if (strcmp (cliente, lista_conta[i]==0)){
+
+        printf("Cliente: %s", cliente.nome);
+        printf("CPF: %s", cliente.cpf);
+        printf("Cliente: %s", cliente.telefone);
+
+        }
+    }
+}
+
+#endif // CLIENTE_H_INCLUDED
