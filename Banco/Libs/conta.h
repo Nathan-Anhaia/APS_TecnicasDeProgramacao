a9f4f83712adf46dbb5b1cfb3232292f1398c55c
Nathan-Anhaia <55770434+Nathan-Anhaia@users.noreply.github.com>
2019-11-05 13:39:20 -0300

    Update conta.h

--- Banco/Libs/conta.h
+++ Banco/Libs/conta.h
@@ -35,33 +35,44 @@ scanf ("%d", &conta);
             saldoDeposito.saldo += valor;
 
             printf("Valor depositado com Sucesso ! \n");
-            printf("Valor depositado: %.2f", valor); 
+            printf("Valor depositado: %.2f", valor);
         }
     }
 }
-int retirar(TConta conta, float quantia)
+int debitar(TConta conta, float quantia)
 
 {
+    TConta leiaDebitar;
+    for (leiaDebitar=0; leiaDebitar <=10; leiaDebitar++)
+    {
+        if (leiaDebitar.numero=listarClientes[leiaDebitar].numero)
+        {
+            printf("Digite o valor para debitar: \n");
+            scanf ("%f",&quantia);
 
-    printf("Digite o valor para debitar: \n");
-    scanf ("%f",&quantia);
+            TConta saldoDebitado;
 
-    TConta saldoDebitado;
+            saldoDebitado.saldo -= quantia;
 
-    saldoDebitado.saldo -= quantia;
-
-    printf("Valor depositado: %.2f", quantia);
-
+            printf("Valor depositado: %.2f", quantia);
+        }
+    }
 }
 int transferir(TConta origem, TConta destino, float quantia)
 {
-    printf("Digite o CPF da sua conta: \n");
-    scanf("%d", &origem);
+    TConta leiaOrigem, leiaDestino;
 
-    printf("Digite o CPF da conta para qual sera feita a transferencia\n");
-    scanf("%d", &destino);
-    printf("Digite a quantia que sera transferida\n");
-    scanf("%f", quantia);
+    for (leiaOrigem=0; leiaOrigem<10; leiaOrigem++)
+    {
+        if
+        printf("Digite o CPF da sua conta: \n");
+        scanf("%d", &origem);
+
+        printf("Digite o CPF da conta para qual sera feita a transferencia\n");
+        scanf("%d", &destino);
+        printf("Digite a quantia que sera transferida\n");
+        scanf("%f", quantia);
+    }
 
 }
 void imprimirConta(TConta conta){
@@ -72,6 +83,5 @@ void imprimirConta(TConta conta){
     TConta SaldoImprimir;
 
     printf("Saldo Total: %.2f \n", SaldoImprimir.saldo);
-    
-     
+
 }
