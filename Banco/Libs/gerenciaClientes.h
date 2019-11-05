f86358ed7588d827cb487c98cf4546a97709b0b5
Nathan-Anhaia <55770434+Nathan-Anhaia@users.noreply.github.com>
2019-11-05 13:40:11 -0300

    Update gerenciaClientes.h

--- Banco/Libs/gerenciaClientes.h
+++ Banco/Libs/gerenciaClientes.h
@@ -1,8 +1,30 @@
-#include "cliente.h"
+#include "stdlib.h"
 
 TCliente clientes[10];
 int totalClientes =0;
 
-void adicionarCliente(TCliente cliente);
-int existeCPF(TCliente cliente);
+void adicionarCliente(TCliente clientes){
+
+    for (int i = 0; i<10; i++){
+        if (clientes != lista_cliente[i]){
+            if (lista_cliente[i] == '\0'){
+            strcpy (lista_cliente[i], clientes[i]);
+            }
+        }else {
+            printf("Cliente nao cadastrado ! \n");
+        }
+}
+int existeCPF(TCliente clientes.cpf){
+
+    for (int j = 0; j<10; j++){
+
+        if (strcmp (lista_cliente[j].cpf, clientes.cpf) == 0){
+
+            printf("Ja existe um usuario com este CPF !\n");
+        } else{
+
+            printf("Nao existe este CPF !\n")
+        }
+    }
+}
 char* listarClientes();
