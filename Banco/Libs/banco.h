0427e34761c8fe24b87b3bdbb3d3ecf92b0858ab
Nathan-Anhaia <55770434+Nathan-Anhaia@users.noreply.github.com>
2019-11-05 13:37:10 -0300

    Update banco.h

--- Banco/Libs/banco.h
+++ Banco/Libs/banco.h
@@ -3,7 +3,16 @@
 TConta contas[10];
 int numeroConta=0;
 
-void adicionarConta(TConta conta);
+void adicionarConta(TConta conta){
+
+    for (int i = 0; i < 10; i++){
+
+        if (lista_conta[i] == ' '){
+
+            strcpy (lista_conta[i], conta);
+        }
+    }
+}
 char* listarContas();
 char* pesquisarConta(int numero);
 TConta getConta(int numero);
