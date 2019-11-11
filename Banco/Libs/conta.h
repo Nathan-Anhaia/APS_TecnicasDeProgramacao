3c1d29d7dc9b319b91b14f18bb00979e40e2eebf
Victor Leite <victorsalvetti@hotmail.com>
2019-11-11 02:49:04 -0300

    Update conta.h

--- Banco/Libs/conta.h
+++ Banco/Libs/conta.h
@@ -1,87 +1,61 @@
-#include "cliente.h"
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef CONTA_H_INCLUDED
+#define CONTA_H_INCLUDED
 
-typedef struct Data{
-int dia;
-int mes;
-int ano;
-}TData;
+#include "cliente.h"
+
+// typedef struct Data{
+//     int dia;
+//     int mes;
+//     int ano;
+// }TData;
 
 typedef struct Conta{
-int numero;
-TCliente Cliente;
-TData dataAbertura;
-float saldo;
-int ativa;
+    int numero, ativa;
+    char dataAbertura[10];
+    float saldo;
 }TConta;
 
-void depositar(TConta conta,float valor){
+TConta conta[100];
 
-int leiaDeposito;
-
-printf("Digite a conta para depositar: \n");
-scanf ("%d", &conta);
-
-    for (leiaDeposito=0; leiaDeposito<=10; leiaDeposito++){
-
-        if (conta.numero == lista_conta[leiaDeposito].numero)
-        {
-            printf("Digite o valor para depositar: \n");
-            scanf ("%f",&valor);
-
-            TConta saldoDeposito;
-
-            saldoDeposito.saldo += valor;
-
-            printf("Valor depositado com Sucesso ! \n");
-            printf("Valor depositado: %.2f", valor);
-        }
-    }
-}
-int debitar(TConta conta, float quantia)
-
+void depositar(int id, float valor)
 {
-    TConta leiaDebitar;
-    for (leiaDebitar=0; leiaDebitar <=10; leiaDebitar++)
+    conta[id].saldo+=valor;
+
+    printf("\nR$%.2f depositados na conta de %s com sucesso! \nSaldo Atual: R$%.2f", valor, cliente[id].nome, conta[id].saldo);
+}
+
+
+int debitar(int id, float valor)
+{
+    if (conta[id].saldo>=valor)
     {
-        if (leiaDebitar.numero=listarClientes[leiaDebitar].numero)
-        {
-            printf("Digite o valor para debitar: \n");
-            scanf ("%f",&quantia);
-
-            TConta saldoDebitado;
-
-            saldoDebitado.saldo -= quantia;
-
-            printf("Valor depositado: %.2f", quantia);
-        }
+        conta[id].saldo-=valor;
+        printf("\nR$%.2f retirados da conta de %s com sucesso! \nSaldo Atual: R$%.2f", valor, cliente[id].nome, conta[id].saldo);
+    }
+    else
+    {
+        printf("Erro: Saldo insuficiente.");
     }
 }
+
+/*
 int transferir(TConta origem, TConta destino, float quantia)
 {
-    TConta leiaOrigem, leiaDestino;
+    int leiaOrigem, leiaDestino;
 
     for (leiaOrigem=0; leiaOrigem<10; leiaOrigem++)
     {
-        if
-        printf("Digite o CPF da sua conta: \n");
-        scanf("%d", &origem);
+        if () {
+            printf("Digite o CPF da sua conta: \n");
+            scanf("%d", &origem);
 
-        printf("Digite o CPF da conta para qual sera feita a transferencia\n");
-        scanf("%d", &destino);
-        printf("Digite a quantia que sera transferida\n");
-        scanf("%f", quantia);
+            printf("Digite o CPF da conta para qual sera feita a transferencia\n");
+            scanf("%d", &destino);
+            printf("Digite a quantia que sera transferida\n");
+            scanf("%f", quantia);
+        }
     }
 
 }
-void imprimirConta(TConta conta){
-
-    printf("Digite o CPF da conta: \n");
-    scanf("%d", &conta);
-
-    TConta SaldoImprimir;
-
-    printf("Saldo Total: %.2f \n", SaldoImprimir.saldo);
-
-}
+*/
+#endif // CONTA_H_INCLUDED
